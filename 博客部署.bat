aa97a61cc0a8f430f0a493c1e46e520209d00dba
Taoweidong <taowd@outlook.com>
2020-04-06 15:11:48 +0800

    博客更新

--- 博客部署.bat
+++ 博客部署.bat
@@ -3,30 +3,15 @@ E:
 cd E:\repTaoweidongGithubIo
 
 REM 本地开发测试
-REM hexo s
+call hexo clean
+call hexo g
+call hexo d
 
-REM 备份博客，以码云的为主
-Xcopy E:\taoweidongBlog\source E:\repTaoweidongGithubIo\source  /s /e /y
-
-REM 部署github博客
-cd E:\repTaoweidongGithubIo
-REM hexo clean&&hexo g&&hexo d
-
-REM 提交码云博客
-cd E:\taoweidongBlog
 REM git --version
-git add -A
-git commit -m "博客更新"
-git push origin master
+call git add -A
+call git commit -m "博客更新"
+call git push origin master
 REM git branch -u origin/master master
-
-REM 开始提交github仓库的博客
-cd E:\repTaoweidongGithubIo
-REM git --version
-git add -A
-git commit -m  "博客更新"
-git push origin master
-REM git branch -u origin/master master
-
+echo GitHub更新成功
 
 pause
